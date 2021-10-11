ded2690d2a0a706dfa3b7dc2608cf41a5a57429e
DerThorsten <derthorstenbeier@gmail.com>
2021-10-11 12:12:28 +0200

    experimenting

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+// Copyright (c) Thorsten Beier
+// Distributed under the terms of the Modified BSD License.
+import { XeusServerKernelBase } from './xeus_server_kernel';
+export default XeusServerKernelBase;
