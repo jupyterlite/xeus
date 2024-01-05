3d6ad61e7fbb1c2ea0b3f7545f9591411e42d697
DerThorsten <derthorstenbeier@gmail.com>
2024-01-05 09:07:51 +0100

    more options

--- jupyterlite_xeus/prefix_bundler/noop_prefix_bundler.py
+++
@@ -1,9 +0,0 @@
-from .prefix_bundler_base import PrefixBundlerBase
-
-# do nothing at all
-class NoopPrefixBundler(PrefixBundlerBase):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def build(self):
-       yield None
