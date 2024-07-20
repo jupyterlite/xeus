3384129656d8dd750a11a9de63f777710d842b8f
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2024-07-20 08:49:57 +0200

    Add file system test

--- ui-tests/tests/jupyterlite_xeus.spec.ts
+++ ui-tests/tests/jupyterlite_xeus.spec.ts
@@ -25,9 +25,6 @@ test.describe('General Tests', () => {
   test('xeus-python should execute some code', async ({ page }) => {
     await page.goto('lab/index.html');
 
-    // Launch a Python notebook
-    const xpython = page.locator('[title="Python 3.11 (XPython)"]').first();
-    await xpython.click();
 
     // Wait for kernel to be idle
     await page.locator('#jp-main-statusbar').getByText('Idle').waitFor();
@@ -43,5 +40,26 @@ test.describe('General Tests', () => {
     expect(await cell?.screenshot()).toMatchSnapshot(
       'jupyter-xeus-execute.png'
     );
+  });
+
+  test('the kernel should have access to the file system', async ({ page }) => {
+    await page.goto('lab/index.html');
+
+    // Create a Python notebook
+    const xpython = page.locator('[title="Python 3.11 (XPython)"]').first();
+    await xpython.click();
+
+    await page.notebook.save();
+
+    await page.notebook.setCell(0, 'code', 'import os; os.listdir()');
+    await page.notebook.runCell(0);
+
+    // Wait for kernel to be idle
+    await page.locator('#jp-main-statusbar').getByText('Idle').waitFor();
+
+    const cell = await page.notebook.getCellOutput(0);
+    const cellContent = await cell?.textContent();
+    const name = 'Untitled.ipynb';
+    expect(cellContent).toContain(name);
   });
 });
