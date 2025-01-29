504586975294f19ddc94a55abca054fb2ed11b7e
martinRenou <martin.renou@gmail.com>
2025-01-29 16:48:51 +0100

    Debug

--- jupyterlite_xeus/create_conda_env.py
+++ jupyterlite_xeus/create_conda_env.py
@@ -171,6 +171,7 @@ def _create_config(prefix_path):
 
 def _get_python_version(json_str):
     for entry in [entry for entry in json.loads(json_str)["actions"]["LINK"] if entry["name"] == "python"]:
+        raise RuntimeError(str(entry))
         python_version = entry["version"]
         python_version = python_version[:python_version.rfind(".")]
         return python_version
