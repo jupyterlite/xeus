8849f8d3e04ee1b8046384f634f7d8458f417796
arjxn-py <arjxn.py@gmail.com>
2025-07-25 18:15:50 +0530

    Fix incompatibility between xeus-cpp/xeus-r and scipy

--- packages/xeus/src/worker.ts
+++ packages/xeus/src/worker.ts
@@ -8,7 +8,7 @@ import {
   bootstrapEmpackPackedEnvironment,
   bootstrapPython,
   getPythonVersion,
-  loadShareLibs,
+  loadSharedLibs,
   ISolvedPackages,
   installPackagesToEmscriptenFS,
   removePackagesFromEmscriptenFS,
@@ -179,12 +179,7 @@ export abstract class EmpackedXeusRemoteKernel extends XeusRemoteKernelBase {
     }
 
     // Load shared libs
-    await loadShareLibs({
-      sharedLibs: this._sharedLibs,
-      prefix: this._prefix,
-      Module: this.Module,
-      logger: this.logger
-    });
+    await this._maybeLoadSharedLibs();
   }
 
   /**
@@ -331,14 +326,28 @@ export abstract class EmpackedXeusRemoteKernel extends XeusRemoteKernelBase {
     });
     this._paths = { ...newPath, ...paths };
 
-    await loadShareLibs({
-      sharedLibs,
-      prefix: this._prefix,
-      Module: this.Module,
-      logger: this.logger
-    });
+    await this._maybeLoadSharedLibs();
 
     this._env = newEnv;
+  }
+
+  private async _maybeLoadSharedLibs() {
+    // If we're running in the Python kernel, load all cpython shared libs beforehand
+    if (this._pythonVersion) {
+      const cpythonSharedLibs: TSharedLibsMap = {};
+      for (const pkgName of Object.keys(this._sharedLibs)) {
+        cpythonSharedLibs[pkgName] = this._sharedLibs[pkgName].filter(
+          sharedLib => sharedLib.includes('cpython-3')
+        );
+      }
+
+      await loadSharedLibs({
+        sharedLibs: cpythonSharedLibs,
+        prefix: '/',
+        Module: this.Module,
+        logger: this.logger
+      });
+    }
   }
 
   private _pythonVersion: number[] | undefined;
