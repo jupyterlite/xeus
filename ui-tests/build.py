74cd7e491b963f1d97253b0062efb43cd1f2eb2c
martinRenou <martin.renou@gmail.com>
2025-09-01 13:11:20 +0200

    Adding more advanced tests

--- ui-tests/build.py
+++ ui-tests/build.py
@@ -4,7 +4,7 @@ from subprocess import run
 import jupyterlab
 
 extra_labextensions_path = str(Path(jupyterlab.__file__).parent / "galata")
-cmd = f"jupyter lite build --FederatedExtensionAddon.extra_labextensions_path={extra_labextensions_path}"
+cmd = f"jupyter lite build --contents notebooks --FederatedExtensionAddon.extra_labextensions_path={extra_labextensions_path}"
 
 run(
     cmd,
